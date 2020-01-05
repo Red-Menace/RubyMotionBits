ba0349e50e870e988b01c1c254076bd793919f45
Red-Menace <red_menace@mac.com>
2020-01-04 18:36:46 -0700

    Update colorHighlight.rb

--- Methods/ Text/colorHighlight.rb
+++ Methods/ Text/colorHighlight.rb
@@ -1,9 +1,10 @@
 
-# Add a text color attribute (error, etc) to attributed text.
-# The matchItem is a regular string or an array of strings.
-# The attribute is added to all matches.
+# Add a text color attribute (error, etc) to portions of attributed text.
+# The matchItem is a string or an array of strings, or if nil, the attributed text string.
+# The attribute is added to all portions that match.
 # Returns the attributed text.
-def colorHighlight(attrText, matchItem, color)
+def colorHighlight(attrText, matchItem, color = NSColor.systemRedColor)
+   matchItem = attrText.string if matchItem.nil?  # match the whole thing
    Array(matchItem).each do |matchText|
       length = matchText.length
       index = -1

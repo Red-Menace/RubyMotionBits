fe9061343baf1b88326710941f7242f016220de6
Red-Menace <red_menace@mac.com>
2020-07-24 12:09:16 -0600

    Update resolve alias.rb

--- Methods/ File/resolve alias.rb
+++ Methods/ File/resolve alias.rb
@@ -1,19 +1,22 @@
 
-# get the original item of an alias
-# Returns the original item or 'unable' string
+# Try to get the original item of an alias.
+# Returns the original item or an "unable to resolve" string.
 def resolveAlias(aliasPath)
-   # try #1
+   # first try
    aliasURL = NSURL.fileURLWithPath(aliasPath)
    errorPointer = Pointer.new(:object)
    bookmarkData = NSURL.bookmarkDataWithContentsOfURL(aliasURL, error: errorPointer)
-   unless bookmarkData  # try #2
+   unless bookmarkData
+      # try again (2)
       symlink = aliasPath.stringByResolvingSymlinksInPath
+      return symlink if symlink != aliasPath
+      # and again (3)
+      symlink = NSFileManager.defaultManager
+                             .destinationOfSymbolicLinkAtPath(aliasPath, error: nil)
       return symlink if symlink != aliasPath
       return "Unable to resolve alias for '#{aliasPath}'"
    end
-   # try #3
-   stalePointer = Pointer.new(:bool)
-   stalePointer[0] = false
+   stalePointer = Pointer.new(:boolean)
    targetURL = NSURL.URLByResolvingBookmarkData( bookmarkData,
                                         options: NSURLBookmarkResolutionWithoutUI,
                                   relativeToURL: aliasURL,

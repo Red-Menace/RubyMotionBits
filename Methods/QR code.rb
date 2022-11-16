7db33da237f3d5a91f4e4e16f05dd560f4b68ecd
Red-Menace <Red-Menace@users.noreply.github.com>
2022-11-15 17:54:01 -0700

    Update QR code.rb

--- Methods/QR code.rb
+++ Methods/QR code.rb
@@ -1,16 +1,36 @@
 
-# Make a QR code from the input text, saving the image to a posixFile.
-def makeQRcode(text, width, posixFile)
-   textData = text.dataUsingEncoding(NSUTF8StringEncoding)
-   imageFilter = CIFilter.filterWithName('CIQRCodeGenerator')
-   imageFilter.setDefaults
-   imageFilter.setValue(textData, forKey: 'inputMessage')
-   imageFilter.setValue('L', forKey: 'inputCorrectionLevel')
-   image = imageFilter.outputImage
-   scale = width / image.extent.size.width
-   transform = CGAffineTransform.CGAffineTransformMakeScale(scale, scale)
-   image.imageByApplyingTransform(transform)
-        .TIFFRepresentation
-        .writeToFile(posixFile, atomically: true)
-end
+   # Create QR code from the input text, saving the image to an output file.
+   def makeQRcode(inputText, outputFile, options = {})
+      imageSize = options.fetch(:imageSize, 256)
+      correctionLevel = options.fetch(:correctionLevel, nil)
+      textData = inputText.dataUsingEncoding(NSUTF8StringEncoding)
+      imageFilter = CIFilter.filterWithName('CIQRCodeGenerator')
+      imageFilter.setDefaults
+      imageFilter.setValue(textData, forKey: 'inputMessage')
+      if !correctionLevel.nil? && ['L', 'M', 'Q', 'H'].include?(correctionLevel)
+         imageFilter.setValue(correctionLevel, forKey: 'inputCorrectionLevel')
+      end
+      image = imageFilter.outputImage
+      scale = imageSize / image.extent.size.width
+      transform = CGAffineTransform.CGAffineTransformMakeScale(scale, scale)
+      image.imageByApplyingTransform(transform)
+           .TIFFRepresentation
+           .writeToFile(outputFile, atomically: true)
+   end
+   
+   
+   # Return text of the QR code from the input File.
+   def readQRcode(inputFile)
+      fileURL = NSURL.fileURLWithPath(inputFile)
+      image = CIImage.imageWithContentsOfURL(fileURL)
+      if (image = CIImage.imageWithContentsOfURL(fileURL))
+         options = {CIDetectorAccuracy: 'CIDetectorAccuracyHigh'}
+         detector = CIDetector.detectorOfType( 'CIDetectorTypeQRCode',
+                                      context: nil,
+                                      options: options )
+         features = detector.featuresInImage(image, options:options)
+         return features[0].messageString unless Array(features).empty?
+      end
+      nil
+   end
 

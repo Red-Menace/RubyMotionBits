68995eed51859202011803744b5a2ad9ed1effd5
Red-Menace <Red-Menace@users.noreply.github.com>
2022-11-19 09:43:44 -0700

    Update osascript.rb

--- Shell/osascript.rb
+++ Shell/osascript.rb
@@ -2,21 +2,25 @@
 # Simple:
 
 # Run an AppleScript via the osascript shell utility.
-# Returns an array consisting of an error flag and the result/error text
-def osascript(script)
-   result = `osascript -s o -e '#{script}'`.chomp  # get the result (or error message)
-   error = $?.exitstatus != 0  # set error flag
-   [error, result]
+# Returns the result text or nil if an error.
+def runScript(script)
+   result = `osascript -s o -e '#{script}'`.chomp
+   if $?.exitstatus != 0  # script error
+      puts result  # just log it
+      nil
+   else
+      result
+   end
 end
 
 
-# A bit more robust:
+# A few more options:
 
 # Run an AppleScript via the osascript shell utility.
 # Arguments to a run handler are in the args parameter, items are escaped for the shell.
-# Returns an array consisting of an error flag and the result text.
-# Note that although the ending newline of the osascript result is stripped, chomp isn't
-# used since some items can end with a return ("\r", or 0D), such as file names.
+# Returns an array consisting of an error boolean and the result text.
+# Note that the ending newline of the result is stripped, but chomp isn't
+# used since items such as file names can end with a return ("\r", or 0D).
 def osascript(script, *args)
    arguments = ''
    args.each do |item|  # escape for the shell
@@ -24,8 +28,6 @@ def osascript(script, *args)
       item.gsub!(/\n/, "'\n'")
       arguments << ' ' + item
    end
-   result = `osascript -s o -e '#{script}'#{arguments}`[0..-2]  # result or error message
-   error = $?.exitstatus != 0  # set error flag
-   [error, result]
+   [$?.exitstatus != 0, `osascript -s o -e '#{script}'#{arguments}`[0..-2]]
 end
 
